15c0cad8f0f647fb3f4dcee8ff778b0d50987164
Crxckovich <artpeshov281402@gmail.com>
2023-11-16 18:15:06 +0300

    228

---
+++ DZ.py
@@ -0,0 +1,22 @@
+# A
+
+# numb = input('Букви').split()
+# print(len(set(numb)))
+
+# B
+
+# spisagopden = set(input('Букви').split())
+# icho2 = set(input('Букви').split())
+# print(len(spisagopden.intersection(icho2)))
+
+# F
+
+# with open('input.txt', 'r', encoding='utf-8') as file:
+#     text = file.read()
+# bukvi = text.split()
+# razniybukvi = set(bukvi)
+# bukvi_chitat = len(razniybukvi)
+# print(bukvi_chitat)
+
+# c
+
